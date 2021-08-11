82f4ca7e99a1bcc3f13ea7403b8bb5ee13541d31
imdanwera <im.danwera@gmail.com>
2021-08-12 02:22:43 +0530

    added changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/hi', (req, res) => {
-  res.send('Hello My World!')
+  res.send('Hello All World!')
 })
 
 app.listen(process.env.PORT || 3000)
