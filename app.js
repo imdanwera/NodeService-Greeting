f06b9918105c5c4aee4a207797728adcd8267136
imdanwera <im.danwera@gmail.com>
2021-08-12 16:44:53 +0530

    added changes

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ app.get('/hi', (req, res) => {
   res.send('Hello All World!')
 })
 
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to the Greeting Service</h1>')
+})
+
 app.listen(process.env.PORT || 3000)
 module.export = app
